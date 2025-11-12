534b9aedb0ef5df9315f8733df91b1aed84ddb30
Red691 <kumarnikhil05848@gmail.com>
2025-11-12 13:36:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
 from bot import Bot
+
 Bot().run()
-
-from flask import Flask
-import threading, time
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running on Choreo!"
-
-def run_web():
-    app.run(host="0.0.0.0", port=8000)
-
-threading.Thread(target=run_web).start()
-
-print("Bot is running on Choreo...")
-
-while True:
-    time.sleep(3600)
