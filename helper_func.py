11e93e0f7207af0b350a3d119e0a565ddb5ad825
Red691 <kumarnikhil05848@gmail.com>
2025-11-18 03:02:16 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -7,17 +7,18 @@ from config import ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
-        
+
 async def encode(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.urlsafe_b64encode(string_bytes)
     base64_string = (base64_bytes.decode("ascii")).strip("=")
     return base64_string
 
+
 async def decode(base64_string):
-    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
+    base64_string = base64_string.strip("=")   # old links containing '=' handled
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
+    string_bytes = base64.urlsafe_b64decode(base64_bytes)
     string = string_bytes.decode("ascii")
     return string
 
@@ -44,4 +45,6 @@ def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-       
+# 🔥 ADMIN CHECK FUNCTION (Tumne jo mangaya)
+async def admin(_, __, message):
+    return message.from_user.id in ADMINS
