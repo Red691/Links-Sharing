03f41174924b3d067b00982191fa9395b5165cb8
Red691 <kumarnikhil05848@gmail.com>
2025-11-28 09:14:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import asyncio
 from bot import Bot
 
-async def main():
-    bot = Bot()
-    await bot.start()               # No run(), no use_qr crash
-    await asyncio.Event().wait()    # Python async idle
-
-if __name__ == "__main__":
-    asyncio.run(main())
+Bot().run()
