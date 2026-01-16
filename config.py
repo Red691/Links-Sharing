921086b57c84fda57b3e64e1fe5e757a2274ad6c
Red691 <kumarnikhil05848@gmail.com>
2026-01-16 15:42:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 import os
+import re
 from os import environ
 import logging
 from logging.handlers import RotatingFileHandler
 
+
+id_pattern = re.compile(r'^.\d+$')
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002716491516 -1002463907537 -1003639224538 -1002942665973').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+
+
+
 #Recommended
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "0")
-APP_ID = int(os.environ.get("APP_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "0")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
 
 ##---------------------------------------------------------------------------------------------------
 
@@ -17,7 +25,7 @@ PORT = os.environ.get("PORT", "8010")
 ##---------------------------------------------------------------------------------------------------
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://reelcraft99:reelcraft99reelcraft@cluster0.f0sv73o.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "links-sharing")
 
 ##---------------------------------------------------------------------------------------------------
@@ -27,7 +35,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "") #No Need keep it blank
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5090651635").split()):
+    for x in (os.environ.get("ADMINS", "5770911041").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -42,7 +50,7 @@ USER_REPLY_TEXT = None
 
 #Admin == OWNERID
 ADMINS.append(OWNER_ID)
-ADMINS.append(5090651635)
+ADMINS.append(5770911041)
 
 ##---------------------------------------------------------------------------------------------------
 
